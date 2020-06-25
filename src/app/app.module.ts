4945e20799ab5bc77d878fab8afbe5b43f41287e
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-25 21:13:22 +0700

    import reactive form, HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductFormComponent,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   imports: [
     BrowserModule,
