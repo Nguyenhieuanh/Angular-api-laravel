6e0b34fbf02a8c156f97d2f67622deaad7949776
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-26 15:50:09 +0700

    create update form templeta

--- src/app/components/update-form/update-form.component.ts
+++ src/app/components/update-form/update-form.component.ts
@@ -1,4 +1,8 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from 'src/app/product.interface';
 import { Component, OnInit } from '@angular/core';
+import { ICategory } from 'src/app/category.interface';
 
 @Component({
   selector: 'app-update-form',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-form.component.css']
 })
 export class UpdateFormComponent implements OnInit {
-
-  constructor() { }
+  categoriesList: ICategory[];
+  product: IProduct;
+  updateForm: FormGroup;
+  constructor(private productService: ProductService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // this.productService.getProductById
+
+    this.updateForm = this.formBuilder.group({
+      name: [],
+      description: [],
+      price: [],
+      category_id: [],
+    });
+  }
+
+  onSubmit() {
+    //
   }
 
 }
