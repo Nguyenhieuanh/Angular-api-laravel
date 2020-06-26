d00b1e8274a56cf26f3ac51163edcd1337015d45
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-26 16:02:07 +0700

    update function ngOnInit update form

--- src/app/components/update-form/update-form.component.ts
+++ src/app/components/update-form/update-form.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from 'src/app/services/product.service';
 import { IProduct } from 'src/app/product.interface';
 import { Component, OnInit } from '@angular/core';
 import { ICategory } from 'src/app/category.interface';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-update-form',
@@ -13,16 +14,30 @@ export class UpdateFormComponent implements OnInit {
   categoriesList: ICategory[];
   product: IProduct;
   updateForm: FormGroup;
-  constructor(private productService: ProductService, private formBuilder: FormBuilder) { }
+  constructor(private productService: ProductService,
+              private formBuilder: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
-    // this.productService.getProductById
-
     this.updateForm = this.formBuilder.group({
       name: [],
       description: [],
       price: [],
       category_id: [],
+    });
+
+    this.productService.getCategories().subscribe(res => {
+      this.categoriesList = res;
+    });
+
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.productService.getProductById(id).subscribe(res => {
+      this.product = res;
+      this.updateForm.patchValue(this.product);
+    }, error => {
+      console.log(error);
     });
   }
 
